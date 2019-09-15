355d24d84f80d1f596c8c70dd9e0f233d960ddfa
elfogre <eb.jose@gmail.com>
2019-09-15 22:42:21 +0200

    New imgs. New menu

--- js/index.js
+++ js/index.js
@@ -6,36 +6,4 @@ function colorImage(imgId,color) {
     imgElement.src = 'imgs/' + imgId+ '/' + imgId + color + '.png';
 }
 
-// changing color of capsule on select event
-document.getElementById('sel_A').onchange = function(){
-    colorImage('A', this.value);
-}
-document.getElementById('sel_B').onchange = function(){
-    colorImage('B', this.value);
-}
-
-document.getElementById('sel_pad').onchange = function(){
-    colorImage('pad', this.value);
-}
-
-document.getElementById('sel_tip').onchange = function(){
-    colorImage('tip', this.value);
-}
-
-document.getElementById('sel_bottomclose').onchange = function(){
-    colorImage('bottomclose', this.value);
-}
-
-document.getElementById('sel_bottomstrip').onchange = function(){
-    colorImage('bottomstrip', this.value);
-}
-
-document.getElementById('sel_topclose').onchange = function(){
-    colorImage('topclose', this.value);
-}
-
-document.getElementById('sel_topstrip').onchange = function(){
-    colorImage('topstrip', this.value);
-}
-
 
